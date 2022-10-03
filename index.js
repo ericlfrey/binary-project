3efaaa3b1b0d7f34b9395b11e53538ce8e465038
Eric Frey <ericlfrey@gmail.com>
2022-10-03 09:50:12 -0500

    Added comments to clarify functions. Added event listeners for Button

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const outputField = document.getElementById('output');
 const inputField = document.getElementById('input');
 const inputButton = document.getElementById('convert');
 const clearMessage = document.getElementById('clear')
-const buttonTest = () => {
+
+const buttonPress = () => {
   const inputValue = document.getElementById('input').value;
   const binary = parseInt(inputValue).toString(2);
   if (inputValue === '' || NaN) {
@@ -16,29 +17,53 @@ const buttonTest = () => {
   clearMessage.innerHTML = `Press "c" to clear`
   return binary;
 }
+
+// clears the input field
 const clearField = () => {
   document.getElementById('input').value = '';
 }
+
+//clears the text field and the 'clear' message
 const clearSpan = () => {
   document.getElementById('output').innerHTML = '';
   document.getElementById('clear').innerHTML = '';
 }
+
+// Enter Key for Input Field
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    buttonTest();
+    buttonPress();
     clearField();
   }
 });
+
+// "c" button press to clear for the Input Field
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "c") {
     event.preventDefault();
     clearSpan();
   }
 });
+
+// "c" button to clear for the Button
 inputButton.addEventListener("keypress", function (event) {
   if (event.key === "c") {
     event.preventDefault();
     clearSpan();
   }
+});
+
+// Enter Key for the Button
+inputButton.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    buttonPress();
+  }
+});
+
+// Mouse Click for Button
+inputButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  buttonPress();
 });
